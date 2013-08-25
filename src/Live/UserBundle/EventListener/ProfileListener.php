efe153c67efacfe7e10007126510c74206ca3933
Aurelien David <aurel.spyl@gmail.com>
2013-08-25 19:15:49 +0200

    add profile listener

---
+++ src/Live/UserBundle/EventListener/ProfileListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Live\UserBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to change the redirection at the end of the password resetting
+ */
+class ProfileListener implements EventSubscriberInterface
+{
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::PROFILE_EDIT_SUCCESS => 'onProfileEditSuccess',
+        );
+    }
+
+    public function onProfileEditSuccess(FormEvent $event)
+    {
+        $url = $this->router->generate('fos_user_profile_edit');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
